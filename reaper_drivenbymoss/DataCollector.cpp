c5491e68ebb28baaeb58b35b86349d833fb3ed0f
git-moss <juergen.mossgraber@googlemail.com>
2019-01-27 22:55:52 +0100

    Send number of tracks first.

--- reaper_drivenbymoss/DataCollector.cpp
+++ reaper_drivenbymoss/DataCollector.cpp
@@ -185,6 +185,7 @@ void DataCollector::CollectTrackData(std::stringstream &ss, ReaProject *project,
 	int count = CountTracks(project);
 	int trackIndex{ 0 };
 	int trackState{};
+	this->model.trackCount = Collectors::CollectIntValue(ss, "/track/count", this->model.trackCount, trackIndex, dump);
 	for (int index = 0; index < count; index++)
 	{
 		MediaTrack *mediaTrack = GetTrack(project, index);
@@ -198,7 +199,6 @@ void DataCollector::CollectTrackData(std::stringstream &ss, ReaProject *project,
 		track->CollectData(ss, project, mediaTrack, trackIndex, dump);
 		trackIndex++;
 	}
-	this->model.trackCount = Collectors::CollectIntValue(ss, "/track/count", this->model.trackCount, trackIndex, dump);
 }
 
 /**
