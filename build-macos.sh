4f95e8f8a9aeb13fb4034f64d4f14397b4a89ff3
mos <mos@UbuntuStudioLenovo>
2021-10-30 12:01:28 +0200

    Fixed cmake on Linux.

--- build-macos.sh
+++ build-macos.sh
@@ -1,5 +1,5 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-16.jdk/Contents/Home
 
 mkdir -p build
-cmake -S "CMake" -B "build" -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ 
-cmake --build "build" --verbose --config Release
+cmake -S "CMake" -B "build" -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Release
+cmake --build "build" --verbose
--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 export JAVA_HOME=/home/mos/java/jdk-16.0.2+7/
 
 mkdir -p build
-cmake -S "CMake" -B "build"
-cmake --build "build" --config Release
-
-mv build/libreaper_drivenbymoss.so build/reaper_drivenbymoss.so
+cmake -S "CMake" -B "build" -D CMAKE_BUILD_TYPE=Release
+cmake --build "build" --verbose
