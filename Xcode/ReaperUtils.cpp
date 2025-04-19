168f62db41b2763c1c0abf669a2b23df9b618d36
mos <mos@mini-von-jurgen.fritz.box>
2025-04-19 20:57:37 +0200

    Added static variable to fix Macos build.

---
+++ Xcode/ReaperUtils.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2018-2025 by Jürgen Moßgraber (www.mossgrabers.de)
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+#include "ReaperUtils.h"
+
+HWND ReaperUtils::mainWindowHandle = nullptr;
